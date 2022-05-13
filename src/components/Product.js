0ffebb86ddf69d7b595c5064d8d6e5f5b806ec4e
Mariana Ito <marianaitopinto@gmail.com>
2022-05-13 14:48:49 -0300

    layout and css

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,19 +4,19 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { Oval } from "react-loader-spinner";
 
+import returnIcon from "../assets/iconreturn.png"
 import API_LINK from '../data/links';
 
 export default function Product() {
     const { productId } = useParams();
     const [loading, setLoading] = useState(true);
     const [product, setProduct] = useState([]);
+    const [size, setSize] = useState("gQty")
 
     useEffect(() => {
         const promise = axios.get(`${API_LINK}/product/${productId}`);
         promise.then((res) => {
-            console.log(res.data);
             setProduct(res.data);
-            console.log(product);
             setLoading(false);
         })
         promise.catch(() => {
@@ -24,6 +24,27 @@ export default function Product() {
             setLoading(false);
         })
     }, []);
+
+    function mappingProductQty(n) {
+        const qty = []
+        for (let i = 0; i <= n; i++) {
+            qty.push(i)
+        }
+        return qty;
+    }
+
+    /*<Selection>
+                        <p onClick={setSize("pQty")}>P</p>
+                        <p onClick={setSize("mQty")}>M</p>
+                        <p onClick={setSize("gQty")}>G</p>
+                        <select>
+                            {mappingProductQty(product[size]).map((qty) =>
+                                <option>
+                                    {qty}
+                                </option>
+                            )}
+                        </select>
+                    </Selection>*/
 
     return (
         loading ? (
@@ -35,23 +56,117 @@ export default function Product() {
             </>
         ) : (
             <>
-                <Loading key={product.name}>
+                <Content>
+                    <>
+                    <img src={returnIcon} alt="Seta para retornar" className="icon"></img>
                     <img src={product.url} alt="Imagem do produto"></img>
-                    <p>{product.name}</p>
+                    <h1>{product.name}</h1>
+                    <h2>R$ {product.price.$numberDecimal}</h2>
                     <p>{product.description}</p>
-                    <p>R$ {product.price.$numberDecimal}</p>
-                </Loading>
+                    <Selection>
+                    <p>P</p>
+                    <p>M</p>
+                    <p>G</p>
+                        <select>
+                            {mappingProductQty(product[size]).map((qty) =>
+                                <option>
+                                    {qty}
+                                </option>
+                            )}
+                        </select>
+                    </Selection>
+                    <Footer>
+                        <button>Adicionar ao carrinho</button>
+                    </Footer>
+                    </>
+                </Content>
             </>
         )
     )
 }
 
-const Loading = styled.div`
+const Loading = styled.body`
+    height:100vh;
     display: flex;
     flex-direction: column;
     align-items: center;
 p {
         font-size: 20px;
         padding: 15px;
+    }
+`
+
+const Content = styled.div`
+    width: 375px;   
+    height:100vh;
+    background:#ffffff;
+    display: flex;
+    flex-direction: column;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 22px;
+
+    img {
+        width: 375px;   
+    }
+
+    h1 {
+        color: #4F4F4F;
+        padding: 5px;
+    }
+
+    h2 {
+        color: #2D7AEF;
+        padding: 5px;
+    }
+
+    p {
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 20px;
+        letter-spacing: -0.165px;
+        color: #4F4F4F;
+        opacity: 0.9;
+        padding: 5px;
+    }
+
+    button {
+        width: 311px;
+        height: 48px;
+        background: #2D7AEF;
+        border-radius: 24px;
+        font-weight: 600;
+        font-size: 18px;
+        line-height: 18px;
+        color: #FFFFFF;
+    }
+
+    .icon {
+        width: 24px;
+        height: 24px;
+        opacity: 0.6;
+        position:absolute;
+    }
+`
+
+const Footer = styled.footer`
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+`
+const Selection = styled.div`
+    display:flex;
+    margin-top: 15px;
+
+    p {
+        border: 1px solid #2D7AEF;
+        border-radius: 80%;
+        column-gap: 10px;
+        margin-right: 10px;
+    }
+
+    select {
+        margin-left: 20px;
+        width: 50px;
     }
 `
